689678547dc15af1dcb28f584a4f25ac19479bba
Jay-Jiang <1540491116@qq.com>
2020-01-15 14:52:44 +0800

    create xxl-job admin(spring+springMVC+mybatis) and executor

---
+++ xxl-job-admin-core/src/main/java/com/xxl/job/admin/controller/interceptor/WebMvcConfig.java
@@ -0,0 +1,28 @@
+package com.xxl.job.admin.controller.interceptor;
+
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.annotation.Resource;
+
+/**
+ * web mvc config
+ *
+ * @author xuxueli 2018-04-02 20:48:20
+ */
+// @Configuration 取消
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Resource
+    private PermissionInterceptor permissionInterceptor;
+    @Resource
+    private CookieInterceptor cookieInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(permissionInterceptor).addPathPatterns("/**");
+        registry.addInterceptor(cookieInterceptor).addPathPatterns("/**");
+        super.addInterceptors(registry);
+    }
+
+}
